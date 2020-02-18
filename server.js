b26bc8a2798fa504d6bf081bd1a32611cd31f592
Vitor Rubim <vitorrubim1@hotmail.com>
2020-02-18 20:33:14 -0300

    Adicionando arquivos estáticos a pasta public

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+//servidor
 const express = require ("express");
 const server = express();
 
+//configurando o servidor para apresentar arquivos estáticos
+server.use(express.static('public')) //adicionando arquivos estáticos a pasta public
+
+// configurando a template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("./", {
+    express: server
+})
+
 server.get("/", function(req, res){
-    return res.send("Hello world!")
+    return res.render("index.html") //renderizando o html
 });
 
+//configurando a porta 3000
 server.listen(3000, function(){
     console.log("rodando...")
 });
