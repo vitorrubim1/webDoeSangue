0faf3ab90156dbcc39a2cacff741cd442b9ed098
Vitor Rubim <vitorrubim1@hotmail.com>
2020-03-20 20:10:15 -0300

    feito toda estrutura front-end, servidor com nodejs e o create do crud, usando o sgbd postbird

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 //servidor
 const express = require ("express");
 const server = express();
+const xlsxtojson = require("xlsx-to-json");
+const xlstojson = require("xls-to-json");
+
 
 //configurando o servidor para apresentar arquivos estáticos
 server.use(express.static('public')) //adicionando arquivos estáticos a pasta public
@@ -8,17 +11,15 @@ server.use(express.static('public')) //adicionando arquivos estáticos a pasta p
 //habilitando o body do form
 server.use(express.urlencoded({ extended: true }));
 
-
 //configurando a conexão com o banco de dados
 const Pool = require("pg").Pool //(pool) mantém a conexão ativa
 const db = new Pool({
     user: 'postgres',
-    password: '1234',
+    password: '1234',   
     host: 'localhost',
     port: '5432',
     database: 'doe',
 });
-
 
 
 // configurando a template engine (que permite enviar dados pro front-end)
@@ -28,23 +29,21 @@ nunjucks.configure("./", {
     noCache: true, //rejeitando o cache
 });
 
-
-
 //PEGANDO DADO DO FORMULÁRIO
 server.post("/", function(req, res){
     //req (requisiçao), buscando ao submitar
     const name = req.body.name;
-    const email = req.body.email;
-    const blood = req.body.blood;
+    const image = req.body.image;
+    const description = req.body.description;
 
-    if (name == "" || email == "" || blood == ""){
+    if (name == "" || image == "" || description == ""){
         return res.send("TODO OS CAMPOS SÃO OBRIGATÓRIOS !!!")
     }
         const query = `
-            INSERT INTO donors ("name", "email", "blood") 
+            INSERT INTO pokemons ("name", "image", "description") 
             VALUES ($1, $2, $3)`; //esse metódo de por os values traz segurança
     
-        const values = [name, email, blood];
+        const values = [name, image, description];
     
         db.query(query, values, function(err){
             if(err) return res.send ("Erro ao inserir no BD");
@@ -58,16 +57,45 @@ server.post("/", function(req, res){
 
 server.get("/", function(req, res){
     
-    db.query("SELECT * FROM donors", function(err, result){
+    db.query("SELECT * FROM pokemons", function(err, result){
         if (err)  return res.send("Erro ao buscar dados do banco");
    
-        const donors = result.rows;
-        return res.render("index.html", { donors } ); //renderizando o html e passando o array
+        const pokemons = result.rows;
+        return res.render("index.html", { pokemons } ); //renderizando o html e passando o array
     });
 
 });
 
+server.use(function(req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Max-Age", "3600");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    next();
+});
+// configuration
+server.use(express.static(__dirname + '/public'));                
+server.use('/public/uploads',express.static(__dirname + '/public/uploads'));       
+
+server.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+server.post('/api/xlstojson', function(req, res) {
+	xlsxtojson({
+		input: "./excel-to-json.xlsx",  // input xls 
+	    output: "output.json", // output json 
+	    lowerCaseHeaders:true
+	}, function(err, result) {
+	    if(err) {
+	      res.json(err);
+	    } else {
+	      res.json(result);
+	    }
+	});
+});
+
 //configurando a porta 3000
 server.listen(3000, function(){
-    console.log("rodando...")
+    console.log("run...")
 });
